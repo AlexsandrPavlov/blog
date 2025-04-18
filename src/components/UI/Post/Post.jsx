3fd3f3903c31a4166056b980e674147f145548e9
Alexsandr Pavlov <buterfeed@gmail.com>
2025-04-18 12:53:23 +0300

    Refactor Post component styles and structure
    - Updated `Post.jsx` to improve readability and maintainability.
    - Modified `Post.module.css` to enhance styling consistency.

--- src/components/UI/Post/Post.jsx
+++ src/components/UI/Post/Post.jsx
@@ -8,20 +8,28 @@ export const Post = () => {
       <section className={style.post_header}>
         <section className={style.post_header_about}>
           <a className={style.post_title}>Some article title</a>
-          <HeartFilled style={{color: 'red'}} />
+          <HeartFilled style={{color: 'red'}} className={style.icon_like} />
           <span className={style.post_likes}>12</span>
           <br />
-          <Tag>Tag 1</Tag>
+          <Tag className={style.tag_name}>Tag 1</Tag>
+          <Tag className={style.tag_name}>Tag 1</Tag>
+          <Tag className={style.tag_name}>Tag 1</Tag>
         </section>
         <section className={style.post_header_user}>
           <section className={style.post_header_user_info}>
-            <span className={style.user_name}>User Name</span>
-            <span className={style.user_date}>Date</span>
+            <span className={style.user_name}>John Doe</span>
+            <span className={style.user_date}>March 5 , 2020</span>
           </section>
-
           <Avatar size={46} icon={<UserOutlined />} />
         </section>
       </section>
+      <div className={style.post_content}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus iaculis. Donec sed odio
+          dui. Nulla vitae elit libero, a pharetra augue. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam
+          quis risus eget urna mollis ornare vel eu leo.
+        </p>
+      </div>
     </div>
   );
 };
